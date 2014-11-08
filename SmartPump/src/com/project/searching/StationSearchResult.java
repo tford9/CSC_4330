601dbb56e45334ec6df0813503f107f1581aaa4a
robry1227 <robry1227@gmail.com>
2014-11-07 18:27:56 -0600

    Add comments for station search results but not sure what to write for the comment methods to implement for sort

--- SmartPump/src/com/project/searching/StationSearchResult.java
+++ SmartPump/src/com/project/searching/StationSearchResult.java
@@ -5,44 +5,92 @@ import java.util.Comparator;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+/** 
+* StationSearchResult lists the Results of the Gas Stations 
+* located within a 5 mile radius of the location. This class
+* implements the Parcelable and Comparable interfaces, which are required
+* since they are dependencies of the GasStation class.
+*
+* @author SmartPump Team
+* @version 1.0
+* @see GasStation
+*/
+
 public class StationSearchResult implements Parcelable, Comparable<StationSearchResult>
 {
     private GasStation station;
     private double adjustedCost;
     
+    /** 
+    * Constructs a StationSearchResult from the GasStation
+    * and the adjustedCost
+    *
+    * @param station The Gas Station
+    * @param adjustedCost The adjusted cost
+    */
     public StationSearchResult(GasStation station, double adjustedCost)
     {
         this.station = station;
         this.adjustedCost = adjustedCost;
     }
     
+    /**
+    * Constructs a StationSearchResult from the Parcel object
+    *
+    * @param in the in Parcel
+    */
     public StationSearchResult(Parcel in)
     {
         adjustedCost = in.readDouble();
         station = in.readParcelable(GasStation.class.getClassLoader());
     }
 
+    /**
+    * Returns the gas station 
+    *
+    * @return station the gas station
+    */
     public GasStation getStation()
     {
         return this.station;
     }
     
+    /**
+    * Returns the adjusted cost for the gas station selected
+    *
+    * @return adjustedCost the adjusted cost
+    */
     public double getAdjustedCost()
     {
         return this.adjustedCost;
     }
     
+    /**
+    * Returns bit masks which can be used to represent significant objects for this Parcel.
+    * Required implementation, but functionality is not currently necessary.
+    *
+    * @return bit mask for the Parcel
+    */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+    * Flattens the adjusted cost's data into the Parcel
+    *
+    * @param dest the destination Parcel
+    * @param flags additional flags specifying how the object should be written
+    */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeDouble(getAdjustedCost());
         dest.writeParcelable(getStation(), flags);
     }
     
+    /**
+    * Implemented interface for generating StationSearchResult instances from a Parcel
+    */
     public static final Parcelable.Creator<StationSearchResult> CREATOR = new Parcelable.Creator<StationSearchResult>() 
     {
         public StationSearchResult createFromParcel(Parcel in) 
